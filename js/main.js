da8a0ffb84df2126a3a659cfae6cf985561336fb
michele <michele.giovagnoli1@gmail.com>
2023-03-10 16:25:01 +0100

    snack1 array di oggetti.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const bici = [
+    {
+        nome:'Cannondale',
+        peso: '7KG'
+    }, {
+        nome:'Factor',
+        peso: '8KG'
+    }, {
+        nome:'Pinarello',
+        peso: '9KG'
+    }
+];
