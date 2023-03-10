607f22199c15f124420990b16a0b0c0bae3f0591
michele <michele.giovagnoli1@gmail.com>
2023-03-10 16:59:23 +0100

    snack 1 stampo la bici piu leggera

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 const bici = [
     {
         nome:'Cannondale',
-        peso: '7KG'
+        peso: '7'
     }, {
         nome:'Factor',
-        peso: '8KG'
+        peso: '8'
     }, {
         nome:'Pinarello',
-        peso: '9KG'
+        peso: '9'
     }
 ];
+const domBici = document.querySelector('.bici');
+bici.forEach(element => {
+    const {peso,nome} = element;
+    if(peso < 8){
+        const biciPeso = `<span>${nome}</span> </br> <span>dal peso di :${peso}kg</span>`;
+        domBici.innerHTML = biciPeso;
+        console.log(peso);
+    }
+});
