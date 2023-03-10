5ffd50624a957e6d5eef1c5ebe71df7d70fca5f8
michele <michele.giovagnoli1@gmail.com>
2023-03-10 17:47:24 +0100

    snack1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,77 @@
 const bici = [
     {
         nome:'Cannondale',
-        peso: '7'
+        peso: '3'
     }, {
         nome:'Factor',
-        peso: '8'
+        peso: '1'
     }, {
         nome:'Pinarello',
         peso: '9'
     }
 ];
+let biciPiccola = 100;
+let nomeBici = '';
 const domBici = document.querySelector('.bici');
 bici.forEach(element => {
     const {peso,nome} = element;
-    if(peso < 8){
-        const biciPeso = `<span>${nome}</span> </br> <span>dal peso di :${peso}kg</span>`;
-        domBici.innerHTML = biciPeso;
-        console.log(peso);
+    nomeBici=nome;
+    //let m = Math.min(peso);
+    //console.log(m);
+    if(peso < biciPiccola){
+        biciPiccola = peso;
     }
+    
 });
+        const biciPeso = `<span>${nomeBici}</span> </br> <span>dal peso di :${biciPiccola}kg</span>`;
+        domBici.innerHTML = biciPeso;
+       // console.log(peso);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const squadra = [
+    {
+        nome:'inter',
+        punti_fatti: '0',
+        falli_subiti:'0'
+    }, {
+        nome:'milane',
+        punti_fatti: '0',
+        falli_subiti:'0'
+    }, {
+        nome:'roma',
+        punti_fatti: '0',
+        falli_subiti:'0'
+    }
+];
+for
+function generateRandomNumber(min, max){
+    const randomNumber = Math.floor((Math.random() * (max - min + 1)) + min);
+    return randomNumber;
+}
+
+function generateUniqueRandomNumber(array, min, max){
+    let value = false;
+    let uniqueRandomNumber;
+    while( !value ){
+        uniqueRandomNumber = generateRandomNumber(min, max);
+        if(!array.includes(uniqueRandomNumber)){
+            value = true; 
+        }
+    }
+    return uniqueRandomNumber;
+}*/
